c585cf96bd3bdb8746cc3a7d10781e86e7d55f4a
MatthewCodez <mayodele113@gmail.com>
2024-11-03 14:22:01 +0100

    localhost audience

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,17 +4,18 @@ import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-     domain="dev-y7agkmqetvk26r0j.us.auth0.com"
-     clientId="vgHbkPBe3b4vcJK5kRtp3uQuGRDhbS4e"
-     authorizationParams={{
-      redirect_uri: "https://solar-project-site.vercel.app"
-     }}
-     audience="https://solar-project-snowy.vercel.app/"
-     scope="openid profile email"
+      domain="dev-y7agkmqetvk26r0j.us.auth0.com"
+      clientId="vgHbkPBe3b4vcJK5kRtp3uQuGRDhbS4e"
+      authorizationParams={{
+        redirect_uri: "https://solar-project-site.vercel.app",
+        // redirect_uri: "window.location.origin"
+      }}
+      // audience="https://solar-project-snowy.vercel.app/"
+      audience="http://localhost:8000"
+      scope="openid profile email"
     >
       <App />
     </Auth0Provider>
