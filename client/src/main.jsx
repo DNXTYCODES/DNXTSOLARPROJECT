51e153aaca680b90fe71ca0de636284360a54ad3
MatthewCodez <mayodele113@gmail.com>
2024-11-03 14:30:03 +0100

    yup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,17 +4,17 @@ import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-y7agkmqetvk26r0j.us.auth0.com"
-      clientId="vgHbkPBe3b4vcJK5kRtp3uQuGRDhbS4e"
-      authorizationParams={{
-        // redirect_uri: "https://solar-project-site.vercel.app"
-        redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.app",
-      }}
-      audience="https://solar-project-snowy.vercel.app/"
-      scope="openid profile email"
+     domain="dev-y7agkmqetvk26r0j.us.auth0.com"
+     clientId="vgHbkPBe3b4vcJK5kRtp3uQuGRDhbS4e"
+     authorizationParams={{
+      redirect_uri: "https://solar-project-site.vercel.app"
+     }}
+     audience="https://solar-project-snowy.vercel.app/"
+     scope="openid profile email"
     >
       <App />
     </Auth0Provider>
