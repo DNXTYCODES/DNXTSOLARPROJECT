6f2e02d5e3348a65f98e2136c85739a11443a77f
MatthewCodez <mayodele113@gmail.com>
2024-11-24 15:13:19 +0100

    new commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://solarbackend-eight.vercel.app/api",
+  baseURL: "solarfrontend-c9rcwk11o-matthewcodezs-projects.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
