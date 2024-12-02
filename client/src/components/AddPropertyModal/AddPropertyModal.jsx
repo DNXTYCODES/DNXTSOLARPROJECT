2ab455b196a5f7515af2b3112cfc4f6393dfc116
MatthewCodez <mayodele113@gmail.com>
2024-12-02 23:36:06 +0100

    OWNER REMOVAL

--- client/src/components/AddPropertyModal/AddPropertyModal.jsx
+++ client/src/components/AddPropertyModal/AddPropertyModal.jsx
@@ -1,3 +1,99 @@
+// // import { Container, Modal, Stepper } from "@mantine/core";
+// // import React, { useState } from "react";
+// // import AddLocation from "../AddLocation/AddLocation";
+// // import { useAuth0 } from "@auth0/auth0-react";
+// // import UploadImage from "../UploadImage/UploadImage";
+// // import BasicDetails from "../BasicDetails/BasicDetails";
+// // import Facilities from "../Facilities/Facilities";
+
+// // const AddPropertyModal = ({ opened, setOpened }) => {
+// //   const [active, setActive] = useState(0);
+// //   const { user } = useAuth0();
+
+// //   const [propertyDetails, setPropertyDetails] = useState({
+// //     title: "",
+// //     description: "",
+// //     price: 0,
+// //     country: "",
+// //     city: "",
+// //     address: "",
+// //     image: null,
+// //     facilities: {
+// //       FirstFeature: "",
+// //       SecondFeature: "",
+// //       ThirdFeature: "",
+// //       FourthFeature: "",
+// //       FifthFeature: "",
+// //     },
+// //     userEmail: user?.email,
+// //   });
+
+// //   const nextStep = () => {
+// //     setActive((current) => (current < 4 ? current + 1 : current));
+// //   };
+
+// //   const prevStep = () => {
+// //     setActive((current) => (current > 0 ? current - 1 : current));
+// //   };
+
+// //   return (
+// //     <Modal
+// //       opened={opened}
+// //       onClose={() => setOpened(false)}
+// //       closeOnClickOutside
+// //       size={"90rem"}
+// //     >
+// //       <Container h={"40rem"} w={"100%"}>
+// //         <Stepper
+// //           active={active}
+// //           onStepClick={setActive}
+// //           breakpoint="sm"
+// //           allowNextStepsSelect={false}
+// //         >
+// //           {/* <Stepper.Step label="Location" description="Address">
+// //             <AddLocation
+// //               nextStep={nextStep}
+// //               propertyDetails={propertyDetails}
+// //               setPropertyDetails={setPropertyDetails}
+// //             />
+// //           </Stepper.Step> */}
+// //           <Stepper.Step label="Images" description="Upload ">
+// //             <UploadImage
+// //               prevStep={prevStep}
+// //               nextStep={nextStep}
+// //               propertyDetails={propertyDetails}
+// //               setPropertyDetails={setPropertyDetails}
+// //             />
+// //           </Stepper.Step>
+// //           <Stepper.Step label="Basics" description="Details">
+// //             <BasicDetails
+// //               prevStep={prevStep}
+// //               nextStep={nextStep}
+// //               propertyDetails={propertyDetails}
+// //               setPropertyDetails={setPropertyDetails}
+// //             />
+// //           </Stepper.Step>
+
+// //           <Stepper.Step>
+// //             <Facilities
+// //               prevStep={prevStep}
+// //               propertyDetails={propertyDetails}
+// //               setPropertyDetails={setPropertyDetails}
+// //               setOpened={setOpened}
+// //               setActiveStep={setActive}
+// //             />
+// //           </Stepper.Step>
+// //           <Stepper.Completed>
+// //             Completed, click back button to get to previous step
+// //           </Stepper.Completed>
+// //         </Stepper>
+// //       </Container>
+// //     </Modal>
+// //   );
+// // };
+
+// // export default AddPropertyModal;
+
 // import { Container, Modal, Stepper } from "@mantine/core";
 // import React, { useState } from "react";
 // import AddLocation from "../AddLocation/AddLocation";
@@ -19,11 +115,9 @@
 //     address: "",
 //     image: null,
 //     facilities: {
-//       FirstFeature: "",
-//       SecondFeature: "",
-//       ThirdFeature: "",
-//       FourthFeature: "",
-//       FifthFeature: "",
+//       bedrooms: 0,
+//       parkings: 0,
+//       bathrooms: 0,
 //     },
 //     userEmail: user?.email,
 //   });
@@ -50,13 +144,13 @@
 //           breakpoint="sm"
 //           allowNextStepsSelect={false}
 //         >
-//           {/* <Stepper.Step label="Location" description="Address">
+//           <Stepper.Step label="Location" description="Address">
 //             <AddLocation
 //               nextStep={nextStep}
 //               propertyDetails={propertyDetails}
 //               setPropertyDetails={setPropertyDetails}
 //             />
-//           </Stepper.Step> */}
+//           </Stepper.Step>
 //           <Stepper.Step label="Images" description="Upload ">
 //             <UploadImage
 //               prevStep={prevStep}
@@ -94,6 +188,8 @@
 
 // export default AddPropertyModal;
 
+
+
 import { Container, Modal, Stepper } from "@mantine/core";
 import React, { useState } from "react";
 import AddLocation from "../AddLocation/AddLocation";
@@ -104,7 +200,6 @@ import Facilities from "../Facilities/Facilities";
 
 const AddPropertyModal = ({ opened, setOpened }) => {
   const [active, setActive] = useState(0);
-  const { user } = useAuth0();
 
   const [propertyDetails, setPropertyDetails] = useState({
     title: "",
@@ -119,7 +214,6 @@ const AddPropertyModal = ({ opened, setOpened }) => {
       parkings: 0,
       bathrooms: 0,
     },
-    userEmail: user?.email,
   });
 
   const nextStep = () => {
@@ -151,7 +245,7 @@ const AddPropertyModal = ({ opened, setOpened }) => {
               setPropertyDetails={setPropertyDetails}
             />
           </Stepper.Step>
-          <Stepper.Step label="Images" description="Upload ">
+          <Stepper.Step label="Images" description="Upload">
             <UploadImage
               prevStep={prevStep}
               nextStep={nextStep}
@@ -178,7 +272,7 @@ const AddPropertyModal = ({ opened, setOpened }) => {
             />
           </Stepper.Step>
           <Stepper.Completed>
-            Completed, click back button to get to previous step
+            Completed, click back button to get to the previous step
           </Stepper.Completed>
         </Stepper>
       </Container>
