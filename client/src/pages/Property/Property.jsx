400e5333c7d268abe79dbf2abe89970b6f1db02b
MatthewCodez <mayodele113@gmail.com>
2024-12-12 21:09:18 +0100

    add to cart without date

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -1,3 +1,191 @@
+// import React, { useContext, useState } from "react";
+// import { useMutation, useQuery } from "react-query";
+// import { useLocation } from "react-router-dom";
+// import { getProperty, removeBooking } from "../../utils/api";
+// import { PuffLoader } from "react-spinners";
+// import { AiFillHeart } from "react-icons/ai";
+// import "./Property.css";
+
+// import { FaShower } from "react-icons/fa";
+// import { AiTwotoneCar } from "react-icons/ai";
+// import { MdLocationPin, MdMeetingRoom } from "react-icons/md";
+// import Map from "../../components/Map/Map";
+// import useAuthCheck from "../../hooks/useAuthCheck";
+// import { useAuth0 } from "@auth0/auth0-react";
+// import BookingModal from "../../components/BookingModal/BookingModal";
+// import UserDetailContext from "../../context/UserDetailContext.js";
+// import { Button } from "@mantine/core";
+// import { toast } from "react-toastify";
+// import Heart from "../../components/Heart/Heart";
+// const Property = () => {
+//   const { pathname } = useLocation();
+//   const id = pathname.split("/").slice(-1)[0];
+//   const { data, isLoading, isError } = useQuery(["resd", id], () =>
+//     getProperty(id)
+//   );
+
+//   const [modalOpened, setModalOpened] = useState(false);
+//   const { validateLogin } = useAuthCheck();
+//   const { user } = useAuth0();
+
+//   const {
+//     userDetails: { token, bookings },
+//     setUserDetails,
+//   } = useContext(UserDetailContext);
+
+//   const { mutate: cancelBooking, isLoading: cancelling } = useMutation({
+//     mutationFn: () => removeBooking(id, user?.email, token),
+//     onSuccess: () => {
+//       setUserDetails((prev) => ({
+//         ...prev,
+//         bookings: prev.bookings.filter((booking) => booking?.id !== id),
+//       }));
+
+//       toast.success("Booking cancelled", { position: "bottom-right" });
+//     },
+//   });
+
+//   if (isLoading) {
+//     return (
+//       <div className="wrapper">
+//         <div className="flexCenter paddings">
+//           <PuffLoader />
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   if (isError) {
+//     return (
+//       <div className="wrapper">
+//         <div className="flexCenter paddings">
+//           <span>Error while fetching the property details</span>
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className="wrapper">
+//       <div className="flexColStart paddings innerWidth property-container">
+//         {/* like button */}
+//         <div className="like">
+//           <Heart id={id}/>
+//         </div>
+
+//         {/* image */}
+//         <img src={data?.image} alt="home image" />
+
+//         <div className="flexCenter property-details">
+//           {/* left */}
+//           <div className="flexColStart left">
+//             {/* head */}
+//             <div className="flexStart head">
+//               <span className="primaryText">{data?.title}</span>
+//               <span className="orangeText" style={{ fontSize: "1.5rem" }}>
+//                 $ {data?.price}
+//               </span>
+//             </div>
+
+//             {/* facilities */}
+//             <div className="flexStart facilities">
+//               {/* bathrooms */}
+//               <div className="flexStart facility">
+//                 <FaShower size={20} color="#1F3E72" />
+//                 <span>{data?.facilities?.bathrooms} Bathrooms</span>
+//               </div>
+
+//               {/* parkings */}
+//               <div className="flexStart facility">
+//                 <AiTwotoneCar size={20} color="#1F3E72" />
+//                 <span>{data?.facilities.parkings} Parking</span>
+//               </div>
+
+//               {/* rooms */}
+//               <div className="flexStart facility">
+//                 <MdMeetingRoom size={20} color="#1F3E72" />
+//                 <span>{data?.facilities.bedrooms} Room/s</span>
+//               </div>
+//             </div>
+
+//             {/* description */}
+
+//             <span className="secondaryText" style={{ textAlign: "justify" }}>
+//               {data?.description}
+//             </span>
+
+//             {/* address */}
+
+//             <div className="flexStart" style={{ gap: "1rem" }}>
+//               <MdLocationPin size={25} />
+//               <span className="secondaryText">
+//                 {data?.address}{" "}
+//                 {data?.city}{" "}
+//                 {data?.country}
+//               </span>
+//             </div>
+
+//             {/* booking button */}
+//             {bookings?.map((booking) => booking.id).includes(id) ? (
+//               <>
+//                 <Button
+//                   variant="outline"
+//                   w={"100%"}
+//                   color="red"
+//                   onClick={() => cancelBooking()}
+//                   disabled={cancelling}
+//                 >
+//                   <span>Remove from Cart</span>
+//                 </Button>
+//                 <span>
+//                   Your visit already booked for date{" "}
+//                   {bookings?.filter((booking) => booking?.id === id)[0].date}
+//                 </span>
+//               </>
+//             ) : (
+//               <button
+//                 className="button"
+//                 onClick={() => {
+//                   validateLogin() && setModalOpened(true);
+//                 }}
+//               >
+//                 Add to Cart
+//               </button>
+//             )}
+
+//             <BookingModal
+//               opened={modalOpened}
+//               setOpened={setModalOpened}
+//               propertyId={id}
+//               email={user?.email}
+//             />
+//           </div>
+
+//           {/* right side */}
+//           <div className="map">
+//             <Map
+//               address={data?.address}
+//               city={data?.city}
+//               country={data?.country}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Property;
+
+
+
+
+
+
+
+
+
+
 import React, { useContext, useState } from "react";
 import { useMutation, useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
@@ -12,11 +200,11 @@ import { MdLocationPin, MdMeetingRoom } from "react-icons/md";
 import Map from "../../components/Map/Map";
 import useAuthCheck from "../../hooks/useAuthCheck";
 import { useAuth0 } from "@auth0/auth0-react";
-import BookingModal from "../../components/BookingModal/BookingModal";
 import UserDetailContext from "../../context/UserDetailContext.js";
 import { Button } from "@mantine/core";
 import { toast } from "react-toastify";
 import Heart from "../../components/Heart/Heart";
+
 const Property = () => {
   const { pathname } = useLocation();
   const id = pathname.split("/").slice(-1)[0];
@@ -33,15 +221,14 @@ const Property = () => {
     setUserDetails,
   } = useContext(UserDetailContext);
 
-  const { mutate: cancelBooking, isLoading: cancelling } = useMutation({
+  const { mutate: removeFromCart, isLoading: removing } = useMutation({
     mutationFn: () => removeBooking(id, user?.email, token),
     onSuccess: () => {
       setUserDetails((prev) => ({
         ...prev,
         bookings: prev.bookings.filter((booking) => booking?.id !== id),
       }));
-
-      toast.success("Booking cancelled", { position: "bottom-right" });
+      toast.success("Removed from cart", { position: "bottom-right" });
     },
   });
 
@@ -59,7 +246,7 @@ const Property = () => {
     return (
       <div className="wrapper">
         <div className="flexCenter paddings">
-          <span>Error while fetching the property details</span>
+          <span>Error while fetching the product details</span>
         </div>
       </div>
     );
@@ -68,18 +255,18 @@ const Property = () => {
   return (
     <div className="wrapper">
       <div className="flexColStart paddings innerWidth property-container">
-        {/* like button */}
+        {/* Like button */}
         <div className="like">
-          <Heart id={id}/>
+          <Heart id={id} />
         </div>
 
-        {/* image */}
-        <img src={data?.image} alt="home image" />
+        {/* Image */}
+        <img src={data?.image} alt="product image" />
 
         <div className="flexCenter property-details">
-          {/* left */}
+          {/* Left section */}
           <div className="flexColStart left">
-            {/* head */}
+            {/* Header */}
             <div className="flexStart head">
               <span className="primaryText">{data?.title}</span>
               <span className="orangeText" style={{ fontSize: "1.5rem" }}>
@@ -87,60 +274,48 @@ const Property = () => {
               </span>
             </div>
 
-            {/* facilities */}
+            {/* Facilities */}
             <div className="flexStart facilities">
-              {/* bathrooms */}
+              {/* Example features */}
               <div className="flexStart facility">
                 <FaShower size={20} color="#1F3E72" />
-                <span>{data?.facilities?.bathrooms} Bathrooms</span>
+                <span>{data?.facilities?.bathrooms} Feature 1</span>
               </div>
-
-              {/* parkings */}
               <div className="flexStart facility">
                 <AiTwotoneCar size={20} color="#1F3E72" />
-                <span>{data?.facilities.parkings} Parking</span>
+                <span>{data?.facilities.parkings} Feature 2</span>
               </div>
-
-              {/* rooms */}
               <div className="flexStart facility">
                 <MdMeetingRoom size={20} color="#1F3E72" />
-                <span>{data?.facilities.bedrooms} Room/s</span>
+                <span>{data?.facilities.bedrooms} Feature 3</span>
               </div>
             </div>
 
-            {/* description */}
-
+            {/* Description */}
             <span className="secondaryText" style={{ textAlign: "justify" }}>
               {data?.description}
             </span>
 
-            {/* address */}
-
+            {/* Address */}
             <div className="flexStart" style={{ gap: "1rem" }}>
               <MdLocationPin size={25} />
               <span className="secondaryText">
-                {data?.address}{" "}
-                {data?.city}{" "}
-                {data?.country}
+                {data?.address} {data?.city} {data?.country}
               </span>
             </div>
 
-            {/* booking button */}
+            {/* Add to Cart button */}
             {bookings?.map((booking) => booking.id).includes(id) ? (
               <>
                 <Button
                   variant="outline"
                   w={"100%"}
                   color="red"
-                  onClick={() => cancelBooking()}
-                  disabled={cancelling}
+                  onClick={() => removeFromCart()}
+                  disabled={removing}
                 >
                   <span>Remove from Cart</span>
                 </Button>
-                <span>
-                  Your visit already booked for date{" "}
-                  {bookings?.filter((booking) => booking?.id === id)[0].date}
-                </span>
               </>
             ) : (
               <button
@@ -152,16 +327,9 @@ const Property = () => {
                 Add to Cart
               </button>
             )}
-
-            <BookingModal
-              opened={modalOpened}
-              setOpened={setModalOpened}
-              propertyId={id}
-              email={user?.email}
-            />
           </div>
 
-          {/* right side */}
+          {/* Right section */}
           <div className="map">
             <Map
               address={data?.address}
@@ -176,3 +344,4 @@ const Property = () => {
 };
 
 export default Property;
+
