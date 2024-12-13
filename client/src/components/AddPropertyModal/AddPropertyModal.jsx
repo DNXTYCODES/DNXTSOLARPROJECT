f9d2f77057915812192498e75886f71761bde929
MatthewCodez <mayodele113@gmail.com>
2024-12-13 22:06:37 +0100

    manual upload logic update

--- client/src/components/AddPropertyModal/AddPropertyModal.jsx
+++ client/src/components/AddPropertyModal/AddPropertyModal.jsx
@@ -190,103 +190,19 @@
 
 
 
-// import { Container, Modal, Stepper } from "@mantine/core";
-// import React, { useState } from "react";
-// import AddLocation from "../AddLocation/AddLocation";
-// import { useAuth0 } from "@auth0/auth0-react";
-// import UploadImage from "../UploadImage/UploadImage";
-// import BasicDetails from "../BasicDetails/BasicDetails";
-// import Facilities from "../Facilities/Facilities";
 
-// const AddPropertyModal = ({ opened, setOpened }) => {
-//   const [active, setActive] = useState(0);
 
-//   const [propertyDetails, setPropertyDetails] = useState({
-//     title: "",
-//     description: "",
-//     price: 0,
-//     country: "",
-//     city: "",
-//     address: "",
-//     image: null,
-//     facilities: {
-//       bedrooms: 0,
-//       parkings: 0,
-//       bathrooms: 0,
-//     },
-//   });
 
-//   const nextStep = () => {
-//     setActive((current) => (current < 4 ? current + 1 : current));
-//   };
 
-//   const prevStep = () => {
-//     setActive((current) => (current > 0 ? current - 1 : current));
-//   };
-
-//   return (
-//     <Modal
-//       opened={opened}
-//       onClose={() => setOpened(false)}
-//       closeOnClickOutside
-//       size={"90rem"}
-//     >
-//       <Container h={"40rem"} w={"100%"}>
-//         <Stepper
-//           active={active}
-//           onStepClick={setActive}
-//           breakpoint="sm"
-//           allowNextStepsSelect={false}
-//         >
-//           <Stepper.Step label="Location" description="Address">
-//             <AddLocation
-//               nextStep={nextStep}
-//               propertyDetails={propertyDetails}
-//               setPropertyDetails={setPropertyDetails}
-//             />
-//           </Stepper.Step>
-//           <Stepper.Step label="Images" description="Upload">
-//             <UploadImage
-//               prevStep={prevStep}
-//               nextStep={nextStep}
-//               propertyDetails={propertyDetails}
-//               setPropertyDetails={setPropertyDetails}
-//             />
-//           </Stepper.Step>
-//           <Stepper.Step label="Basics" description="Details">
-//             <BasicDetails
-//               prevStep={prevStep}
-//               nextStep={nextStep}
-//               propertyDetails={propertyDetails}
-//               setPropertyDetails={setPropertyDetails}
-//             />
-//           </Stepper.Step>
-
-//           <Stepper.Step>
-//             <Facilities
-//               prevStep={prevStep}
-//               propertyDetails={propertyDetails}
-//               setPropertyDetails={setPropertyDetails}
-//               setOpened={setOpened}
-//               setActiveStep={setActive}
-//             />
-//           </Stepper.Step>
-//           <Stepper.Completed>
-//             Completed, click back button to get to the previous step
-//           </Stepper.Completed>
-//         </Stepper>
-//       </Container>
-//     </Modal>
-//   );
-// };
-
-// export default AddPropertyModal;
 
 
 import { Container, Modal, Stepper } from "@mantine/core";
 import React, { useState } from "react";
+import AddLocation from "../AddLocation/AddLocation";
+import { useAuth0 } from "@auth0/auth0-react";
 import UploadImage from "../UploadImage/UploadImage";
 import BasicDetails from "../BasicDetails/BasicDetails";
+import Facilities from "../Facilities/Facilities";
 
 const AddPropertyModal = ({ opened, setOpened }) => {
   const [active, setActive] = useState(0);
@@ -297,6 +213,7 @@ const AddPropertyModal = ({ opened, setOpened }) => {
     price: 0,
     image: null,
   });
+
 
   const nextStep = () => {
     setActive((current) => (current < 2 ? current + 1 : current));
@@ -320,7 +237,13 @@ const AddPropertyModal = ({ opened, setOpened }) => {
           breakpoint="sm"
           allowNextStepsSelect={false}
         >
-          {/* Step 1: Upload Images */}
+          {/* <Stepper.Step label="Location" description="Address">
+            <AddLocation
+              nextStep={nextStep}
+              propertyDetails={propertyDetails}
+              setPropertyDetails={setPropertyDetails}
+            />
+          </Stepper.Step> */}
           <Stepper.Step label="Images" description="Upload">
             <UploadImage
               prevStep={prevStep}
@@ -329,20 +252,26 @@ const AddPropertyModal = ({ opened, setOpened }) => {
               setPropertyDetails={setPropertyDetails}
             />
           </Stepper.Step>
-
-          {/* Step 2: Basic Details */}
           <Stepper.Step label="Basics" description="Details">
             <BasicDetails
               prevStep={prevStep}
               nextStep={nextStep}
               propertyDetails={propertyDetails}
               setPropertyDetails={setPropertyDetails}
-              setOpened={setOpened}
             />
           </Stepper.Step>
 
+          {/* <Stepper.Step>
+            <Facilities
+              prevStep={prevStep}
+              propertyDetails={propertyDetails}
+              setPropertyDetails={setPropertyDetails}
+              setOpened={setOpened}
+              setActiveStep={setActive}
+            />
+          </Stepper.Step> */}
           <Stepper.Completed>
-            Property details submitted successfully!
+            Completed, click back button to get to the previous step
           </Stepper.Completed>
         </Stepper>
       </Container>
